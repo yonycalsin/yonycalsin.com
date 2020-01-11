4ffb477e8be4b755938672d9485f1302dcc4edab
yonicb <yonicb20@gmail.com>
2020-01-10 23:46:35 -0500

    page: 📄 Update main Page

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,102 +15,100 @@ import quotes from '../../data/quotes'
 import tania from '../../content/images/tania-avatar.jpg'
 
 export default class Index extends Component {
-  render() {
-    const { data } = this.props
+   render() {
+      const { data } = this.props
 
-    const latestPostEdges = data.latest.edges
-    const popularPostEdges = data.popular.edges
+      const latestPostEdges = data.latest.edges
+      const popularPostEdges = data.popular.edges
 
-    return (
-      <Layout>
-        <Helmet title={`${config.siteTitle} – Developer & Writer`} />
-        <SEO />
-        <div className="container">
-          <div className="lead">
-            <div className="elevator">
-              <h1>{`Hey, I'm Tania`}</h1>
-              <p>
-                I'm a front end software developer working on{' '}
-                <a href="https://github.com/taniarascia" target="_blank" rel="noopener noreferrer">
-                  open source
-                </a>{' '}
-                and <Link to="/blog">writing</Link> about modern JavaScript, Node.js, and
-                development. 💾
+      return (
+         <Layout>
+            <Helmet title={`${config.siteTitle} – Developer & Writer`} />
+            <SEO />
+            <div className="container">
+               <div className="lead">
+                  <div className="elevator">
+                     <h1>{`Hey, I'm Tania`}</h1>
+                     <p>
+                        Soy un desarrollador de software de primera línea que trabaja en{' '}
+                        <a href="https://github.com/taniarascia" target="_blank" rel="noopener noreferrer">
+                           open source</a>{' '}
+                        y <Link to="/blog">escribiendo</Link> sobre JavaScript moderno, Node.js, y desarrollo. 💾
               </p>
-              <div className="social-buttons">
-                <GitHubButton
-                  href="https://github.com/taniarascia"
-                  data-size="large"
-                  data-show-count="true"
-                >
-                  taniarascia
+                     <div className="social-buttons">
+                        <GitHubButton
+                           href="https://github.com/yonicb"
+                           data-size="large"
+                           data-show-count="true"
+                        >
+                           yonicb
                 </GitHubButton>
-              </div>
-            </div>
-            <div className="newsletter-section">
-              <img src={tania} className="newsletter-avatar" alt="Tania" />
-              <div>
-                <h3>Get updates</h3>
-                <p>Open source projects and development tutorials</p>
-                <a className="button" href="https://taniarascia.substack.com">
-                  Subscribe
+                     </div>
+                  </div>
+                  <div className="newsletter-section">
+                     <img src={tania} className="newsletter-avatar" alt="Tania" />
+                     <div>
+                        <h3>Obtener actualizaciones</h3>
+                        <p>Proyectos de código abierto y tutoriales de desarrollo</p>
+                        <a className="button" href="https://taniarascia.substack.com">
+                           Suscríbete
                 </a>
-              </div>
+                     </div>
+                  </div>
+               </div>
             </div>
-          </div>
-        </div>
 
-        <div className="container front-page">
-          <section className="section">
-            <h2>
-              Latest Articles
+            <div className="container front-page">
+               <section className="section">
+                  <h2>
+                     Últimos artículos
               <Link to="/blog" className="view-all">
-                View all
+                        Ver todo
               </Link>
-            </h2>
-            <PostListing simple postEdges={latestPostEdges} />
-          </section>
+                  </h2>
+                  <PostListing simple postEdges={latestPostEdges} />
+               </section>
 
-          <section className="section">
-            <h2>
-              Most Popular
+               <section className="section">
+                  <h2>
+                     Lo más popular
               <Link to="/categories/popular" className="view-all">
-                View all
+                        Ver todo
               </Link>
-            </h2>
-            <PostListing simple postEdges={popularPostEdges} />
-          </section>
+                  </h2>
+                  <PostListing simple postEdges={popularPostEdges} />
+               </section>
 
-          <section className="section">
-            <h2>Open Source Projects</h2>
-            <ProjectListing projects={projects} />
-          </section>
+               <section className="section">
+                  <h2>Proyectos de código abierto</h2>
+                  <ProjectListing projects={projects} />
+               </section>
 
-          <section className="section">
-            <h2>Interviews</h2>
-            <SimpleListing simple data={podcasts} />
-          </section>
+               {/* <section className="section">
+                  <h2>Entrevistas</h2>
+                  <SimpleListing simple data={podcasts} />
+               </section> */}
 
-          <section className="section">
-            <h2>Talks</h2>
-            <SimpleListing simple data={speaking} />
-          </section>
+               {/* <section className="section">
+                  <h2>Charlas</h2>
+                  <SimpleListing simple data={speaking} />
+               </section> */}
 
-          <section className="section">
-            <h2>{`Other People's Opinions`}</h2>
-            <div className="quotations">
-              {quotes.map(quote => (
-                <blockquote className="quotation" key={quote.name}>
-                  <p>{quote.quote}</p>
-                  <cite>— {quote.name}</cite>
-                </blockquote>
-              ))}
+               <section className="section">
+                  <h2>{`Other People's Opinions`}</h2>
+                  <div className="quotations">
+                     {quotes.map(quote => (
+                        <blockquote className="quotation" key={quote.name}>
+                           <p>{quote.quote}</p>
+                           <cite>— {quote.name}</cite>
+                        </blockquote>
+                     ))}
+                  </div>
+               </section>
             </div>
-          </section>
-        </div>
-      </Layout>
-    )
-  }
+         </Layout>
+      )
+   }
 }
 
 export const pageQuery = graphql`
