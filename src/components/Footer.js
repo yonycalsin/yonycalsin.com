229e71b44b2fe589ec082e2abec41a51d5198c5e
yonicb <yonicb20@gmail.com>
2020-01-10 23:37:05 -0500

    comp: 🍚 Update yoni links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,51 +5,51 @@ import gatsby from '../../content/thumbnails/gatsby.png'
 import github from '../../content/images/github.png'
 
 export default class Footer extends Component {
-  render() {
-    return (
-      <footer className="footer container">
-        <div>
-          <a href="https://ko-fi.com/taniarascia" target="_blank" rel="noopener noreferrer">
-            Ko-Fi
+   render() {
+      return (
+         <footer className="footer container">
+            <div>
+               <a href="https://ko-fi.com/yonicb" target="_blank" rel="noopener noreferrer">
+                  Ko-Fi
           </a>
-          <a href="https://patreon.com/taniarascia" target="_blank" rel="noopener noreferrer">
-            Patreon
+               <a href="https://patreon.com/yonicb" target="_blank" rel="noopener noreferrer">
+                  Patreon
           </a>
-          <Link to="/newsletter">Newsletter</Link>
-          <a href="https://www.taniarascia.com/rss.xml" target="_blank" rel="noopener noreferrer">
-            RSS
+               <Link to="/newsletter">Newsletter</Link>
+               <a href="https://www.greyblu.com/rss.xml" target="_blank" rel="noopener noreferrer">
+                  RSS
           </a>
-        </div>
-        <div>
-          <a href="https://github.com/taniarascia" title="Open-source on GitHub">
-            <img
-              src={github}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="footer-img"
-              alt="GitHub"
-            />
-          </a>
-          <a href="https://www.netlify.com/" title="Hosted by Netlify">
-            <img
-              src={netlify}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="footer-img"
-              alt="GitHub"
-            />
-          </a>
-          <a href="https://www.gatsbyjs.org/" title="Built with Gatsby">
-            <img
-              src={gatsby}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="footer-img"
-              alt="GitHub"
-            />
-          </a>
-        </div>
-      </footer>
-    )
-  }
+            </div>
+            <div>
+               <a href="https://github.com/yonicb" title="Open-source on GitHub">
+                  <img
+                     src={github}
+                     target="_blank"
+                     rel="noopener noreferrer"
+                     className="footer-img"
+                     alt="GitHub"
+                  />
+               </a>
+               <a href="https://www.netlify.com/" title="Hosted by Netlify">
+                  <img
+                     src={netlify}
+                     target="_blank"
+                     rel="noopener noreferrer"
+                     className="footer-img"
+                     alt="GitHub"
+                  />
+               </a>
+               <a href="https://www.gatsbyjs.org/" title="Built with Gatsby">
+                  <img
+                     src={gatsby}
+                     target="_blank"
+                     rel="noopener noreferrer"
+                     className="footer-img"
+                     alt="GitHub"
+                  />
+               </a>
+            </div>
+         </footer>
+      )
+   }
 }
