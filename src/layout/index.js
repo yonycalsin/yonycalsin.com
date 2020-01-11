5b8929fb9ec3ec76c1d651f82350b26bfa814156
yonicb <yonicb20@gmail.com>
2020-01-10 23:41:44 -0500

    layout: 🎴 Index Layout Formating

--- src/layout/index.js
+++ src/layout/index.js
@@ -8,33 +8,33 @@ import favicon from '../images/favicon.png'
 import '../styles/main.scss'
 
 export default class MainLayout extends Component {
-  static contextType = ThemeContext
+   static contextType = ThemeContext
 
-  render() {
-    const { dark, notFound } = this.context
-    const { children } = this.props
-    let themeClass = ''
-    
-    if (dark && !notFound) {
-      themeClass = 'dark'
-    } else if (notFound) {
-      themeClass = 'not-found'
-    }
+   render() {
+      const { dark, notFound } = this.context
+      const { children } = this.props
+      let themeClass = ''
 
-    return (
-      <>
-        <Helmet
-          bodyAttributes={{
-            class: `theme ${themeClass}`,
-          }}
-        >
-          <meta name="description" content={config.siteDescription} />
-          <link rel="shortcut icon" type="image/png" href={favicon} />
-        </Helmet>
-        <Navigation menuLinks={config.menuLinks} />
-        <main id="main-content">{children}</main>
-        <Footer />
-      </>
-    )
-  }
+      if (dark && !notFound) {
+         themeClass = 'dark'
+      } else if (notFound) {
+         themeClass = 'not-found'
+      }
+
+      return (
+         <>
+            <Helmet
+               bodyAttributes={{
+                  class: `theme ${themeClass}`,
+               }}
+            >
+               <meta name="description" content={config.siteDescription} />
+               <link rel="shortcut icon" type="image/png" href={favicon} />
+            </Helmet>
+            <Navigation menuLinks={config.menuLinks} />
+            <main id="main-content">{children}</main>
+            <Footer />
+         </>
+      )
+   }
 }
