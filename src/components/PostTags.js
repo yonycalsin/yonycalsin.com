0594e377c7fede92cf2df0ae1313baf23fe78c52
yonicb <yonicb20@gmail.com>
2020-01-10 23:39:28 -0500

    comp: 🍚 Formating this component

--- src/components/PostTags.js
+++ src/components/PostTags.js
@@ -3,18 +3,18 @@ import kebabCase from 'lodash.kebabcase'
 import { Link } from 'gatsby'
 
 export default class PostTags extends Component {
-  render() {
-    const { tags, size } = this.props
+   render() {
+      const { tags, size } = this.props
 
-    return (
-      <div className="tag-container">
-        {tags &&
-          tags.map(tag => (
-            <Link key={tag} style={{ textDecoration: 'none' }} to={`/tags/${kebabCase(tag)}/`}>
-              <span className={size}>{tag}</span>
-            </Link>
-          ))}
-      </div>
-    )
-  }
+      return (
+         <div className="tag-container">
+            {tags &&
+               tags.map(tag => (
+                  <Link key={tag} style={{ textDecoration: 'none' }} to={`/tags/${kebabCase(tag)}/`}>
+                     <span className={size}>{tag}</span>
+                  </Link>
+               ))}
+         </div>
+      )
+   }
 }
