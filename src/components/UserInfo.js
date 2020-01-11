bc04418e5f4fb05784cc28e5e4f64643c11ddb7b
yonicb <yonicb20@gmail.com>
2020-01-10 23:41:18 -0500

    comp: 🍚 Update change user information

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -4,47 +4,45 @@ import patreon from '../../content/thumbnails/patreon.png'
 import kofi from '../../content/thumbnails/kofi.png'
 
 export default class UserInfo extends Component {
-  render() {
-    return (
-      <aside className="note">
-        <div className="container note-container">
-          <div className="flex-author">
-            <div className="flex-avatar">
-              <img className="avatar" src={tania} alt="Tania Rascia" />
-            </div>
-            <div>
-              <p>
-                I’m Tania Rascia. I document everything I learn and help thousands of people start
-                coding careers.{' '}
-                <strong>
-                  My site has no ads, sponsors, or bullshit. If you enjoy my content, please
-                  consider supporting what I do.
+   render() {
+      return (
+         <aside className="note">
+            <div className="container note-container">
+               <div className="flex-author">
+                  <div className="flex-avatar">
+                     <img className="avatar" src={tania} alt="Yoni Calsin" />
+                  </div>
+                  <div>
+                     <p>
+                        Soy Yoni Calsin. Documento todo lo que aprendo y ayudo a miles de personas a comenzar a codificar carreras.{' '}
+                        <strong>
+                           Si disfrutas de mi contenido, considera apoyar lo que hago.
                 </strong>
-              </p>
+                     </p>
 
-              <div className="flex">
-                <a
-                  href="https://ko-fi.com/taniarascia"
-                  className="donate-button"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={kofi} className="coffee-icon" alt="Coffee icon" />
-                  Buy me a coffee
+                     <div className="flex">
+                        <a
+                           href="https://ko-fi.com/yonicb"
+                           className="donate-button"
+                           target="_blank"
+                           rel="noopener noreferrer"
+                        >
+                           <img src={kofi} className="coffee-icon" alt="Coffee icon" />
+                           Cómprame un café
                 </a>
-                <a
-                  className="patreon-button"
-                  href="https://www.patreon.com/taniarascia"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={patreon} height="50" width="50" alt="Patreon" /> Become a Patron
+                        <a
+                           className="patreon-button"
+                           href="https://www.patreon.com/yonicb"
+                           target="_blank"
+                           rel="noopener noreferrer"
+                        >
+                           <img src={patreon} height="50" width="50" alt="Patreon" /> Conviértase en mi patrocinador
                 </a>
-              </div>
+                     </div>
+                  </div>
+               </div>
             </div>
-          </div>
-        </div>
-      </aside>
-    )
-  }
+         </aside>
+      )
+   }
 }
