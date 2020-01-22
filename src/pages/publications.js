15821da36b426a03c52f00c5244c6755d827e4b2
yonicb <yonicb20@gmail.com>
2020-01-22 00:26:06 -0500

    style: 💄 Formating, and change english to spanish

--- src/pages/publications.js
+++ src/pages/publications.js
@@ -12,58 +12,59 @@ import gatsby from '../../content/thumbnails/gatsby.png'
 import logRocket from '../../content/images/logrocket.png'
 
 export default class PublicationsPage extends Component {
-  render() {
-    const logoMap = {
-      DigitalOcean: digitalOcean,
-      SitePoint: sitePoint,
-      Codrops: codrops,
-      'Envato Tuts+': envato,
-      Progress: progress,
-      Gatsby: gatsby,
-      LogRocket: logRocket,
-    }
+   render() {
+      const logoMap = {
+         DigitalOcean: digitalOcean,
+         SitePoint: sitePoint,
+         Codrops: codrops,
+         'Envato Tuts+': envato,
+         Progress: progress,
+         Gatsby: gatsby,
+         LogRocket: logRocket,
+      }
 
-    const companyStr = Object.keys(logoMap)
-      .join(', ')
-      .replace(/, ([^,]*)$/, ' and $1')
+      const companyStr = Object.keys(logoMap)
+         .join(', ')
+         .replace(/, ([^,]*)$/, ' and $1')
 
-    const pubs = Object.entries(publications)
-    return (
-      <Layout>
-        <Helmet title={`Published Articles – ${config.siteTitle}`} />
-        <div className="container">
-          <header className="page-header">
-            <h1>Publications</h1>
-          </header>
-          <div className="page">
-            <p>I've written for {companyStr}.</p>
-            {pubs.map((publication, i) => {
-              const company = publication[0]
-              const articles = publication[1]
+      const pubs = Object.entries(publications)
+      return (
+         <Layout>
+            <Helmet title={`Artículos publicados – ${config.siteTitle}`} />
+            <div className="container">
+               <header className="page-header">
+                  <h1>Publicaciones</h1>
+               </header>
+               <div className="page">
+                  {/* <p>I've written for {companyStr}.</p> */}
+                  <p>Escrito por {companyStr}.</p>
+                  {pubs.map((publication, i) => {
+                     const company = publication[0]
+                     const articles = publication[1]
 
-              return (
-                <article>
-                  <h2 className="publication-company" id={company.replace(/\s/g, '')}>
-                    <img src={logoMap[company]} alt="Company" />
-                    {company}
-                  </h2>
-                  <ul key={i}>
-                    {articles.map((article, i) => {
-                      return (
-                        <li key={i}>
-                          <a href={article.path} target="_blank" rel="noopener noreferrer">
-                            {article.title}
-                          </a>
-                        </li>
-                      )
-                    })}
-                  </ul>
-                </article>
-              )
-            })}
-          </div>
-        </div>
-      </Layout>
-    )
-  }
+                     return (
+                        <article>
+                           <h2 className="publication-company" id={company.replace(/\s/g, '')}>
+                              <img src={logoMap[company]} alt="Company" />
+                              {company}
+                           </h2>
+                           <ul key={i}>
+                              {articles.map((article, i) => {
+                                 return (
+                                    <li key={i}>
+                                       <a href={article.path} target="_blank" rel="noopener noreferrer">
+                                          {article.title}
+                                       </a>
+                                    </li>
+                                 )
+                              })}
+                           </ul>
+                        </article>
+                     )
+                  })}
+               </div>
+            </div>
+         </Layout>
+      )
+   }
 }
