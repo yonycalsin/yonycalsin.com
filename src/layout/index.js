6b88f089738d70ac7738081d529b9f63a8064b0f
Yoni Calsin <yonicb20@gmail.com>
2020-07-04 15:15:56 -0500

    feat: 🎸 created popup, and backtop styles

--- src/layout/index.js
+++ src/layout/index.js
@@ -6,6 +6,8 @@ import Footer from '../components/Footer';
 import config from '../../data/SiteConfig';
 import favicon from '../images/favicon.png';
 import '../styles/main.scss';
+import Popup from '../components/popup';
+import BackTop from '../components/back-top';
 
 export default class MainLayout extends Component {
    static contextType = ThemeContext;
@@ -31,9 +33,11 @@ export default class MainLayout extends Component {
                <meta name="description" content={config.siteDescription} />
                <link rel="shortcut icon" type="image/png" href={favicon} />
             </Helmet>
+            <Popup />
             <Navigation menuLinks={config.menuLinks} />
             <main id="main-content">{children}</main>
             <Footer />
+            <BackTop />
          </>
       );
    }
