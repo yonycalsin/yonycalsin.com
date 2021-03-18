d3968dd6a3c763f6b7756edf1b7ef5ce0faa5195
Yony Calsin <helloyonycalsin@gmail.com>
2021-03-17 23:10:35 -0500

    feat: 🎸 update navbar

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -17,30 +17,39 @@ export const Navbar = (props: NavbarProps) => {
 
    const { t } = useTranslation()
 
+   const languagesRender = languages.map(lang => (
+      // eslint-disable-next-line jsx-a11y/anchor-is-valid
+      <a
+         key={lang}
+         href="#"
+         onClick={e => {
+            e.preventDefault()
+            changeLanguage(lang)
+         }}
+      >
+         {t(`languages.${lang}`)}
+      </a>
+   ))
+
    return (
       <div className="navbar">
          <div className="container fx fx-aic fx-jcsb">
             <div className="languages-select">
                {isStandaloneBrand ? (
-                  languages.map(lang => (
-                     // eslint-disable-next-line jsx-a11y/anchor-is-valid
-                     <a
-                        key={lang}
-                        href="#"
-                        onClick={e => {
-                           e.preventDefault()
-                           changeLanguage(lang)
-                        }}
-                     >
-                        {t(`languages.${lang}`)}
-                     </a>
-                  ))
+                  languagesRender
                ) : (
                   <Link to="/">Yony Calsin</Link>
                )}
             </div>
             <div className="actions">
                <div className="fx menu">
+                  {!isStandaloneBrand && languagesRender}
+                  {!isStandaloneBrand && (
+                     // eslint-disable-next-line jsx-a11y/anchor-is-valid
+                     <a href="#" style={{ borderBottom: 'none' }}>
+                        |
+                     </a>
+                  )}
                   <Link to="/me/">
                      <Trans>{t('about-me')}</Trans>
                   </Link>
