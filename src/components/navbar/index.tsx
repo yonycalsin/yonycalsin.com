9817b4cebafcb135a442ce7918668219f989d75d
Yony Calsin <helloyonycalsin@gmail.com>
2021-04-19 21:52:33 -0500

    refactor: 💡 remover languages

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,56 +1,17 @@
 import * as React from 'react'
-import {
-   Link,
-   Trans,
-   useI18next,
-   useTranslation,
-} from 'gatsby-plugin-react-i18next'
+import { Link, Trans, useTranslation } from 'gatsby-plugin-react-i18next'
 
-type NavbarProps = {
-   isStandaloneBrand?: boolean
-}
-
-export const Navbar = (props: NavbarProps) => {
-   const { isStandaloneBrand } = props
-
-   const { languages, changeLanguage } = useI18next()
-
+export const Navbar = () => {
    const { t } = useTranslation()
-
-   const languagesRender = languages.map(lang => (
-      // eslint-disable-next-line jsx-a11y/anchor-is-valid
-      <a
-         key={lang}
-         href="#"
-         onClick={e => {
-            e.preventDefault()
-            changeLanguage(lang)
-         }}
-         className="mr-4 text-gray-600 font-semibold"
-      >
-         {t(`languages.${lang}`)}
-      </a>
-   ))
 
    return (
       <div>
          <div className="container prose prose-sm sm:prose lg:prose-lg xl:prose-2xl mx-auto flex items-center justify-between  h-20">
             <div>
-               {isStandaloneBrand ? (
-                  languagesRender
-               ) : (
-                  <Link to="/">Yony Calsin</Link>
-               )}
+               <Link to="/">Yony Calsin</Link>
             </div>
             <div>
                <div>
-                  {!isStandaloneBrand && languagesRender}
-                  {!isStandaloneBrand && (
-                     // eslint-disable-next-line jsx-a11y/anchor-is-valid
-                     <a href="#" className="mr-4">
-                        |
-                     </a>
-                  )}
                   <Link to="/me/" className="mr-4">
                      <Trans>{t('about-me')}</Trans>
                   </Link>
