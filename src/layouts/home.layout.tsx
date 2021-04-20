dbf01418d91e29c46ddfbbaa723063b45d257725
Yony Calsin <helloyonycalsin@gmail.com>
2021-04-19 21:22:13 -0500

    feat: 🎸 add footer

--- src/layouts/home.layout.tsx
+++ src/layouts/home.layout.tsx
@@ -18,6 +18,19 @@ export const HomeLayout = (props: HomeLayoutProps) => {
          <div className="container prose prose-sm sm:prose lg:prose-lg xl:prose-2xl mx-auto">
             {children}
          </div>
+
+         <div className="container sm:prose lg:prose-lg xl:prose-2xl mx-auto border-t-2 border-gray-100 bg-gray-50 mb-10">
+            <div className="flex flex-wrap justify-between items-center ">
+               <small>
+                  Created By{' '}
+                  <i>
+                     <b>Yony Calsin</b>
+                  </i>{' '}
+               </small>
+
+               <small>With Gatsby, Typescript, TailwindCss, Netlify</small>
+            </div>
+         </div>
       </>
    )
 }
