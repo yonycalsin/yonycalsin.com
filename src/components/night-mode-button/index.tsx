95c27bf30f2cc869f1c9d800446d0c3c1812d592
Yony Calsin <helloyonycalsin@gmail.com>
2021-05-09 12:11:07 -0500

    feat: 🎸 create NightModeButton

---
+++ src/components/night-mode-button/index.tsx
@@ -0,0 +1,47 @@
+import * as React from 'react'
+import clsx from 'clsx'
+
+import * as modules from './night-mode-button.module.css'
+
+const STORAGE_KEY = 'color-mode'
+
+const NightModeButton = () => {
+   function onToggleDark() {
+      const isDark = document.documentElement.classList.contains('dark')
+
+      localStorage.setItem(STORAGE_KEY, isDark ? 'light' : 'dark')
+
+      if (isDark) {
+         document.documentElement.classList.remove('dark')
+      } else {
+         document.documentElement.classList.add('dark')
+      }
+   }
+
+   return (
+      <button
+         className="appearance-none outline-none bg-yellow-200 dark:bg-gray-700 rounded-full h-40 w-40 absolute overflow-hidden -left-20 -top-20"
+         onClick={onToggleDark}
+         style={{
+            outline: 'none',
+         }}
+      >
+         <div
+            className="h-36 top-2 bottom-2 flex flex-col items-stretch justify-between absolute w-16 transform rotate-135 dark:-rotate-45 transition-transform duration-500"
+            style={{
+               left: 'calc(50% - 2rem)',
+            }}
+         >
+            <p className={clsx(modules.item, 'w-14 h-14')}></p>
+            <p
+               className={clsx(modules.item, 'w-14 h-14')}
+               style={{
+                  backgroundPosition: 'bottom',
+               }}
+            ></p>
+         </div>
+      </button>
+   )
+}
+
+export default NightModeButton
