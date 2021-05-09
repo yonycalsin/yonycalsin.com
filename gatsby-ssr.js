134d2cf1f9f5665dddd8f6cd5557a4017223d34f
Yony Calsin <helloyonycalsin@gmail.com>
2021-05-09 12:11:37 -0500

    feat: 🎸 add set-initial-color package

---
+++ gatsby-ssr.js
@@ -0,0 +1,12 @@
+const React = require('react')
+
+const HeadComponents = [
+   <script src="https://unpkg.com/@rqbazan/set-initial-color-mode@1.0.1" />,
+]
+
+exports.onRenderBody = (
+   { setHeadComponents, setHtmlAttributes, setBodyAttributes },
+   pluginOptions,
+) => {
+   setHeadComponents(HeadComponents)
+}
