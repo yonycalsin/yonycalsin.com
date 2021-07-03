d9c097bcad12cb07cf58920ef59cf5fefddea04c
Yony Calsin <helloyonycalsin@gmail.com>
2021-07-03 18:38:41 -0500

    feat: 🎸 update the navbar height

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -7,7 +7,7 @@ export const Navbar = () => {
 
    return (
       <div className="dark:bg-gray-900">
-         <div className="container flex items-center justify-between flex-col h-auto md:flex-row md:h-14 lg:h-20">
+         <div className="container flex items-center justify-between flex-col h-auto md:flex-row md:h-7 lg:h-10">
             <div>
                <Link to="/" className="block no-underline">
                   <h1 className="text-2xl line-through text-gray-600">
