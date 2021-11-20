c0f937194825612c7c996692f0d4dfa28be0a844
Yony Calsin <helloyonycalsin@gmail.com>
2021-11-20 08:24:50 -0500

    feat: 🎸 styling the rehypeCodeTitles

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -39,7 +39,12 @@ export default makeSource({
     rehypePlugins: [
       rehypeSlug,
       rehypeCodeTitles,
-      rehypePrism,
+      [
+        rehypePrism,
+        {
+          showLineNumbers: true,
+        },
+      ],
       [
         rehypeAutolinkHeadings,
         {
