a757ea6885a236e257f9c4005a3d95acdcc76408
Yony Calsin <helloyonycalsin@gmail.com>
2021-11-21 11:00:12 -0500

    feat: 🎸 add notRobots to blog page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,14 +1,13 @@
 import * as React from 'react'
 import Head from 'next/head'
 
+import { Meta } from '~/components/meta'
 import { HomeLayout } from '~/layouts'
 
 const NotFoundPage = () => {
   return (
     <HomeLayout>
-      <Head>
-        <title>Page Not Found</title>
-      </Head>
+      <Meta title="Page not found - Yony Calsin" notRobots />
 
       <div className="my-5">
         <div className="my-20">
