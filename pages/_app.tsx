298237b3f87456bf569d373ae8c78c86bd9c92a0
Yony Calsin <helloyonycalsin@gmail.com>
2021-11-25 23:41:05 -0500

    feat: 🎸 add npprogress like bar loader

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,18 @@
 import * as React from 'react'
+import { useRouter } from 'next/dist/client/router'
 import type { AppProps } from 'next/dist/shared/lib/router/router'
+import NProgress from 'nprogress'
 import { DefaultFeature, FeatureProvider } from 'toggled'
 
 import Features from '~/common/features'
 import env from '~/utils/env'
 
 import '~/styles/global.css'
+import '~/styles/nprogress.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
   const features = React.useMemo(() => {
     const data: DefaultFeature[] = []
 
@@ -24,6 +29,29 @@ function MyApp({ Component, pageProps }: AppProps) {
 
     return data
   }, [])
+
+  React.useEffect(() => {
+    const handleStart = () => {
+      NProgress.start()
+    }
+    const handleStop = () => {
+      NProgress.done()
+    }
+
+    router.events.on('routeChangeStart', handleStart)
+
+    router.events.on('routeChangeComplete', handleStop)
+
+    router.events.on('routeChangeError', handleStop)
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart)
+
+      router.events.off('routeChangeComplete', handleStop)
+
+      router.events.off('routeChangeError', handleStop)
+    }
+  }, [router])
 
   return (
     <FeatureProvider features={features}>
