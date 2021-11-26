96794de81f752fbfb2b26e2b72d318b3813cc11a
Yony Calsin <helloyonycalsin@gmail.com>
2021-11-26 18:31:53 -0500

    feat: 🎸 add custom theme to storybook config

---
+++ .storybook/theme.js
@@ -0,0 +1,7 @@
+import { create } from '@storybook/theming'
+
+export default create({
+  base: 'dark',
+  brandTitle: 'Yony Calsin - Personal Website',
+  brandUrl: 'https://www.yonycalsin.com',
+})
