86d2584f5e686db4cdde6f28973752791bc5973e
Yony Calsin <helloyonycalsin@gmail.com>
2021-11-26 19:38:01 -0500

    feat: 🎸 add addon-storysource to storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const tsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
+const prettierConfig = require('../.prettierrc.json')
 
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
@@ -11,6 +12,14 @@ module.exports = {
       options: {
         postcssLoaderOptions: {
           implementation: require('postcss'),
+        },
+      },
+    },
+    {
+      name: '@storybook/addon-storysource',
+      options: {
+        loaderOptions: {
+          prettierConfig,
         },
       },
     },
