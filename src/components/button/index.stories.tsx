56318dd6ddb2d315589bd413025294f482bc91e0
Yony Calsin <helloyonycalsin@gmail.com>
2021-11-26 20:04:24 -0500

    feat: 🎸 add button stories

--- src/components/button/index.stories.tsx
+++ src/components/button/index.stories.tsx
@@ -1,8 +1,30 @@
+import type { ComponentMeta, ComponentStory } from '@storybook/react'
+
 import { Button } from './index'
 
 export default {
   title: 'Core/Button',
   component: Button,
+} as ComponentMeta<typeof Button>
+
+const Template: ComponentStory<typeof Button> = args => <Button {...args} />
+
+export const Default = Template.bind({})
+
+Default.args = {
+  children: 'Hello World',
 }
 
-export const Default = () => <Button>Hello Button</Button>
+export const Disabled = Template.bind({})
+
+Disabled.args = {
+  children: 'Click Me',
+  isDisabled: true,
+}
+
+export const FullWidth = Template.bind({})
+
+FullWidth.args = {
+  children: 'Click Me',
+  isFullWidth: true,
+}
