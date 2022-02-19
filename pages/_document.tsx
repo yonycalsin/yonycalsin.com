0c015bf1a5d591805113b10796e0e59847195b3a
Yony Calsin <helloyonycalsin@gmail.com>
2022-02-19 10:27:44 -0500

    temp: backup

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { Head, Html, Main, NextScript } from 'next/document'
+import Document, { DocumentContext, DocumentInitialProps, Head, Html, Main, NextScript } from 'next/document'
 
 import { isProduction } from '~/utils'
 import { cookieNames } from '~/utils/constants'
@@ -30,24 +30,39 @@ function HeadScripts() {
   )
 }
 
-function MyDocument() {
-  return (
-    <Html>
-      <Head>
-        <script
-          id="theme-mode-script"
-          dangerouslySetInnerHTML={{
-            __html: `!function(){"use strict";var e="string"==typeof(e=localStorage.getItem("${cookieNames.THEME_MODE}"))?e:"boolean"==typeof(e=matchMedia("(prefers-color-scheme: dark)")).matches&&e.matches?"dark":"light";"dark"===e?document.documentElement.classList.add("dark"):document.documentElement.classList.remove("dark"),localStorage.setItem("${cookieNames.THEME_MODE}",e)}();`,
-          }}
-        />
-        {isProduction && <HeadScripts />}
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+class MyDocument extends Document {
+  static override async getInitialProps(ctx: DocumentContext) {
+    // Run the parent `getInitialProps`, it now includes the custom `renderPage`
+    const initialProps = await Document.getInitialProps(ctx)
+
+    console.log(ctx.)
+
+    return initialProps
+  }
+
+  override render() {
+    const pageProps = this.props?.__NEXT_DATA__?.props?.pageProps
+
+    console.log(pageProps)
+
+    return (
+      <Html>
+        <Head>
+          <script
+            id="theme-mode-script"
+            dangerouslySetInnerHTML={{
+              __html: `!function(){"use strict";var e="string"==typeof(e=localStorage.getItem("${cookieNames.THEME_MODE}"))?e:"boolean"==typeof(e=matchMedia("(prefers-color-scheme: dark)")).matches&&e.matches?"dark":"light";"dark"===e?document.documentElement.classList.add("dark"):document.documentElement.classList.remove("dark"),localStorage.setItem("${cookieNames.THEME_MODE}",e)}();`,
+            }}
+          />
+          {isProduction && <HeadScripts />}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
 }
 
 export default MyDocument
---
+++ src/components/resume/index.tsx
@@ -0,0 +1,21 @@
+import ReactPDF from '@react-pdf/renderer'
+
+export async function getStaticProps() {
+  return { props: { isDark: true } }
+}
+
+export default function Resume() {
+  return (
+    <ReactPDF.PDFViewer className="absolute top-0 left-0 w-full h-full">
+      <ReactPDF.Document title="Hello World" author="Yony Calsin">
+        <ReactPDF.Page size="A4">
+          <ReactPDF.View>
+            <ReactPDF.Text>Yony Calsin Resume</ReactPDF.Text>
+          </ReactPDF.View>
+        </ReactPDF.Page>
+      </ReactPDF.Document>
+    </ReactPDF.PDFViewer>
+  )
+}
+
+// Resume.bodyClassName = 'h-full w-full overflow-hidden m-0 bg-[rgb(82, 86, 89)]'
