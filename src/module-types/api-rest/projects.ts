217ee99c1f3008738ca373ca79804dd71d9a4e77
Yony Calsin <helloyonycalsin@gmail.com>
2022-03-18 23:26:26 -0500

    feat: add projects types

---
+++ src/module-types/api-rest/projects.ts
@@ -0,0 +1,45 @@
+export enum IProjectType {
+  PROJECT = 'project',
+  PACKAGE = 'package',
+  CONTRIBUTION = 'contribution',
+}
+
+export enum IProjectStatus {
+  PUBLISHED = 'published',
+  DRAFT = 'draft',
+}
+
+export enum IProjectMaintenanceStatus {
+  ACTIVE = 'active',
+  DEPRECATED = 'deprecated',
+  INACTIVE = 'inactive',
+}
+
+export interface IProjectTechStack {
+  name: string
+  color: string
+}
+
+export interface IProject {
+  id: number
+  name: string
+  type: IProjectType
+  status: IProjectStatus
+  maintenanceStatus: IProjectMaintenanceStatus
+  isFeatured: boolean
+  shortDescription: string
+  websiteUrl: string | null
+  repositoryUrl: string | null
+  packageUrl: string | null
+  techStack: IProjectTechStack[]
+  startedAt: string
+}
+
+export interface IProjectQueryWithMeta {
+  data: IProject[]
+  meta: PaginationMeta
+}
+
+export interface IProjectQueryWithData {
+  data: IProject
+}
