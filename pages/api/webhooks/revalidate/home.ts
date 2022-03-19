044b683ba06fd66423244593dd3679fc1c2bb57f
Yony Calsin <helloyonycalsin@gmail.com>
2022-03-19 13:30:11 -0500

    feat: revalidate home page correctly

--- pages/api/webhooks/revalidate/home.ts
+++ pages/api/webhooks/revalidate/home.ts
@@ -8,7 +8,7 @@ import type HttpResponse from '~/server/apps/client-front-rest/Shared/Http/Defin
 
 async function handler(req: HttpRequest, res: HttpResponse) {
   try {
-    await res.unstable_revalidate('/home')
+    await res.unstable_revalidate('/')
 
     return res.json({ revalidated: true })
   } catch (err) {
