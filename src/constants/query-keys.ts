44a51bc0fb45035c97f22c121c16cc81c08b756a
Yony Calsin <helloyonycalsin@gmail.com>
2022-03-18 23:23:02 -0500

    fix: add QUERY_KEY_PINNED_PROJECTS

--- src/constants/query-keys.ts
+++ src/constants/query-keys.ts
@@ -1,2 +1,9 @@
 // TODO: pass (isFeatured) filter to query
 export const QUERY_KEY_FEATURED_RECOMMENDATIONS = ['/recommendations']
+
+export const QUERY_KEY_PINNED_PROJECTS = [
+  '/projects',
+  {
+    isFeatured: true,
+  },
+]
