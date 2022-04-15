b72a766183154d5c4583b8eea5c0c95d00254355
Yony Calsin <helloyonycalsin@gmail.com>
2022-04-15 18:10:55 -0500

    test: fixes

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,6 @@ const { compilerOptions } = require('./tsconfig')
 const moduleNameMapper = {
   '\\.(css|less|sass|scss)$': 'identity-obj-proxy',
   '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__mocks__/fileMock.js',
-
   ...pathsToModuleNameMapper(compilerOptions.paths),
 }
 
