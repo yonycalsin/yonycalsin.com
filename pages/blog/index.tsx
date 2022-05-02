aa50b161fabed380d4c9804deb18d8be3b835b7a
Yony Calsin <helloyonycalsin@gmail.com>
2022-05-02 18:14:30 -0500

    feat: update the blog layout

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,3 +1,4 @@
+import { Container, Heading, Text, VStack } from '@chakra-ui/react'
 import type { GetStaticPropsResult } from 'next'
 
 import { BlogPosts } from '~/components/blog-post-list/blog-posts'
@@ -14,14 +15,18 @@ function BlogPage(props: BlogPageProps) {
 
   return (
     <>
+      <Meta title="Blog" />
       <MainLayout>
-        <Meta title="Blog" />
-        <h1>Blog - Articles</h1>
-        <p>
-          Tutoriales, artículos técnicos, fragmentos, materiales de referencia y todos los recursos relacionados con el
-          desarrollo que he escrito.
-        </p>
-        <BlogPosts title="All Articles" posts={allPosts} className="mt-4" />
+        <Container maxW="container.md" as="main" py="10">
+          <VStack spacing="6" mb="6">
+            <Heading>Blog</Heading>
+            <Text>
+              Tutoriales, artículos técnicos, fragmentos, materiales de referencia y todos los recursos relacionados con
+              el desarrollo que he escrito.
+            </Text>
+          </VStack>
+          <BlogPosts title="All Articles" posts={allPosts} className="mt-4" />
+        </Container>
       </MainLayout>
     </>
   )
