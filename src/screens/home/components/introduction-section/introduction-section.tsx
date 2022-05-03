220dcb02311bc6a84ea09d699f7992cc81a46863
Yony Calsin <helloyonycalsin@gmail.com>
2022-05-02 21:47:01 -0500

    feat: add social buttons to home page

--- src/screens/home/components/introduction-section/introduction-section.tsx
+++ src/screens/home/components/introduction-section/introduction-section.tsx
@@ -1,9 +1,17 @@
 import * as React from 'react'
-import { BiArrowToRight } from 'react-icons/bi'
-import { Button, Heading, Text, VStack } from '@chakra-ui/react'
-import RouterLink from 'next/link'
+import { BiLinkExternal } from 'react-icons/bi'
+import { TiSocialGithub, TiSocialLinkedin, TiSocialTwitter } from 'react-icons/ti'
+import { Button, Heading, HStack, Text, useBreakpointValue, VStack } from '@chakra-ui/react'
+
+import { socialLinks } from '~/utils/constants'
 
 export function IntroductionSection() {
+  const socialButtonSize = useBreakpointValue({
+    base: 'xs',
+    md: 'sm',
+    lg: 'md',
+  })
+
   return (
     <VStack spacing="6" alignItems="flex-start" py="3">
       <Heading fontWeight="extrabold">Hola, soy Yony</Heading>
@@ -22,11 +30,47 @@ export function IntroductionSection() {
         . Este es mi sitio web personal, donde encontrarás todas las cosas que he aprendido y creado a lo largo de los
         años
       </Text>
-      <RouterLink href="/me" passHref>
-        <Button as="a" rightIcon={<BiArrowToRight />} colorScheme="primary" variant="outline">
-          Más sobre mí
+      <HStack>
+        <Button
+          as="a"
+          href={socialLinks.TWITTER}
+          target="_blank"
+          rel="noreferrer"
+          leftIcon={<TiSocialTwitter />}
+          rightIcon={<BiLinkExternal />}
+          variant="ghost"
+          colorScheme="twitter"
+          size={socialButtonSize}
+        >
+          Twitter
         </Button>
-      </RouterLink>
+        <Button
+          as="a"
+          href={socialLinks.GITHUB}
+          target="_blank"
+          rel="noreferrer"
+          leftIcon={<TiSocialGithub />}
+          rightIcon={<BiLinkExternal />}
+          variant="ghost"
+          colorScheme="primary"
+          size={socialButtonSize}
+        >
+          Github
+        </Button>
+        <Button
+          as="a"
+          href={socialLinks.LINKEDIN}
+          target="_blank"
+          rel="noreferrer"
+          leftIcon={<TiSocialLinkedin />}
+          rightIcon={<BiLinkExternal />}
+          variant="ghost"
+          colorScheme="linkedin"
+          size={socialButtonSize}
+        >
+          Linkedin
+        </Button>
+      </HStack>
     </VStack>
   )
 }
