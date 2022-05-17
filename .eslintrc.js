999ad074764246292c4919996483766fc924c793
Yony Calsin <helloyonycalsin@gmail.com>
2022-05-16 23:52:11 -0500

    feat: install @yonycalsin/eslint-plugin-import-sort

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,12 @@
 module.exports = {
-  plugins: ['@typescript-eslint', 'simple-import-sort', 'testing-library'],
+  plugins: ['@typescript-eslint', 'testing-library'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'next',
     'next/core-web-vitals',
     'prettier',
     'plugin:storybook/recommended',
+    'plugin:@yonycalsin/import-sort/react',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -16,26 +17,6 @@ module.exports = {
     sourceType: 'module',
   },
   rules: {
-    'simple-import-sort/imports': [
-      'error',
-      {
-        groups: [
-          // Side effect imports.
-          ['^\\u0000'],
-          // Packages. `react` related packages come first.
-          ['^react', '^@?\\w'],
-          // Internal modules.
-          ['^~/?\\w'],
-          // Parent imports. Put `..` last.
-          ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
-          // Other relative imports. Put same-folder imports and `.` last.
-          ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
-          // Style imports.
-          ['^.+\\.css$'],
-        ],
-      },
-    ],
-    'simple-import-sort/exports': 'error',
     'no-console': ['warn', { allow: ['warn', 'error'] }],
     '@next/next/no-img-element': 'off',
     '@typescript-eslint/ban-ts-comment': [
