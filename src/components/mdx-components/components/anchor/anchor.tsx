1cc6b78a4e4c72ceb8c5ec9ba1df675c97937e8c
Yony Calsin <helloyonycalsin@gmail.com>
2022-06-05 23:12:23 -0500

    feat: add target blank if the url is external

--- src/components/mdx-components/components/anchor/anchor.tsx
+++ src/components/mdx-components/components/anchor/anchor.tsx
@@ -2,4 +2,16 @@ import * as React from 'react'
 import { chakra } from '@chakra-ui/react'
 
 // eslint-disable-next-line react/display-name, @typescript-eslint/no-explicit-any
-export const Anchor = React.forwardRef((props: any, ref: any) => <chakra.a ref={ref} apply="mdx.a" {...props} />)
+export const Anchor = React.forwardRef((props: any, ref: any) => {
+  const isNotExternal = props.href.startsWith('/')
+
+  return (
+    <chakra.a
+      ref={ref}
+      apply="mdx.a"
+      {...props}
+      target={isNotExternal ? undefined : '_blank'}
+      rel={isNotExternal ? undefined : 'noopener noreferrer'}
+    />
+  )
+})
