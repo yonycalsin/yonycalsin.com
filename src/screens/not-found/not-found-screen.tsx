986f9720b40f0ccc957ccab6f6102a8d06ca7b70
Yony Calsin <helloyonycalsin@gmail.com>
2022-06-06 20:24:21 -0500

    feat: update the page-not-foud screen

--- src/screens/not-found/not-found-screen.tsx
+++ src/screens/not-found/not-found-screen.tsx
@@ -1,17 +1,57 @@
 import * as React from 'react'
-import { Container } from '@chakra-ui/react'
+import { Box, Button, Container, Heading, Text, VStack } from '@chakra-ui/react'
+import RouterLink from 'next/link'
 
 import { MainLayout } from '~/layouts'
 
 export function NotFoundScreen() {
   return (
     <MainLayout>
-      <Container maxW="container.md" as="main" py="10">
-        <div className="my-5">
-          <div className="my-20">
-            <h1 className="text-center my-5">Page Not Found</h1>
-          </div>
-        </div>
+      <Container maxW="container.md" as="main" py="12">
+        <VStack spacing="12">
+          <Heading>😢</Heading>
+          <Heading>Página no encontrada</Heading>
+          <Box
+            display="flex"
+            flexDirection={{
+              base: 'column',
+              md: 'row',
+            }}
+            gap="6"
+          >
+            <RouterLink href="/" passHref>
+              <Button as="a" variant="link" colorScheme="primary">
+                Ir al inicio
+              </Button>
+            </RouterLink>
+            <Text
+              display={{
+                base: 'none',
+                md: 'block',
+              }}
+            >
+              •
+            </Text>
+            <RouterLink href="/me" passHref>
+              <Button as="a" variant="link" colorScheme="primary">
+                Ir a acerca de mí
+              </Button>
+            </RouterLink>
+            <Text
+              display={{
+                base: 'none',
+                md: 'block',
+              }}
+            >
+              •
+            </Text>
+            <RouterLink href="/projects" passHref>
+              <Button as="a" variant="link" colorScheme="primary">
+                Ir a los proyectos
+              </Button>
+            </RouterLink>
+          </Box>
+        </VStack>
       </Container>
     </MainLayout>
   )
