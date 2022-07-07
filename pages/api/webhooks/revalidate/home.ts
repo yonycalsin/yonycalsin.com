30524ec28d2c8ac502dad848c53781e172ffb338
Yony Calsin <helloyonycalsin@gmail.com>
2022-07-06 19:37:18 -0500

    chore: update next (#83)

--- pages/api/webhooks/revalidate/home.ts
+++ pages/api/webhooks/revalidate/home.ts
@@ -8,7 +8,7 @@ import type HttpResponse from '~/server/apps/client-front-rest/Shared/Http/Defin
 
 async function handler(req: HttpRequest, res: HttpResponse) {
   try {
-    await res.unstable_revalidate('/')
+    await res.revalidate('/')
 
     return res.json({ revalidated: true })
   } catch (err) {
