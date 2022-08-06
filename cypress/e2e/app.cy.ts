61326e1b86b24b98a2cc31fe3f7630b605c5156b
Yony Calsin <helloyonycalsin@gmail.com>
2022-08-06 18:35:02 -0500

    chore: update cypress v9 to v10

---
+++ cypress/e2e/app.cy.ts
@@ -0,0 +1,13 @@
+// Disable ESLint to prevent failing linting inside the Next.js repo.
+// If you're using ESLint on your project, we recommend installing the ESLint Cypress plugin instead:
+// https://github.com/cypress-io/eslint-plugin-cypress
+
+describe('Navigation', () => {
+  it('should navigate to the home page', () => {
+    // Start from the index page
+    cy.visit('/')
+  })
+})
+
+// Prevent TypeScript from reading file as legacy script
+export {}
