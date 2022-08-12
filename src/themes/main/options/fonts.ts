9d55e21da129d806c0249701a1b9086d5d950244
Yony Calsin <helloyonycalsin@gmail.com>
2022-08-12 17:36:56 -0500

    refactor: add main-theme options

---
+++ src/themes/main/options/fonts.ts
@@ -0,0 +1,7 @@
+const fonts = {
+  heading: `"Sofia Pro", Inter, -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"`,
+  body: `"Sofia Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"`,
+  mono: `SFMono-Regular,Menlo,Monaco,Consolas,"Liberation Mono","Courier New",monospace`,
+}
+
+export default fonts
