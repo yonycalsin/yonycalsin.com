ff5f8370033203696850af3dc511303a0c2d3621
Yony Calsin <helloyonycalsin@gmail.com>
2022-09-12 20:52:39 -0500

    chore: remove coverage threshold

--- jest.config.js
+++ jest.config.js
@@ -9,14 +9,14 @@ const createJestConfig = nextJest({
 // Add any custom config to be passed to Jest
 const customJestConfig = {
   displayName: '@yonycalsin/yonycalsin.com',
-  coverageThreshold: {
-    global: {
-      statements: 80,
-      branches: 70,
-      functions: 75,
-      lines: 80,
-    },
-  },
+  //   coverageThreshold: {
+  //     global: {
+  //       statements: 80,
+  //       branches: 70,
+  //       functions: 75,
+  //       lines: 80,
+  //     },
+  //   },
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   moduleNameMapper: {
     // Handle module aliases (this will be automatically configured for you soon)
