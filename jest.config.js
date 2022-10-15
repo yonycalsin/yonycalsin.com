6b175b678306b98fe36ba4538ae44133580012b4
Yony Calsin <helloyonycalsin@gmail.com>
2022-10-15 18:18:56 -0500

    test: fix tests

--- jest.config.js
+++ jest.config.js
@@ -7,8 +7,10 @@ const createJestConfig = nextJest({
 })
 
 // Add any custom config to be passed to Jest
+/** @type {import('jest').Config} */
 const customJestConfig = {
   displayName: '@yonycalsin/yonycalsin.com',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   //   coverageThreshold: {
   //     global: {
   //       statements: 80,
@@ -17,31 +19,29 @@ const customJestConfig = {
   //       lines: 80,
   //     },
   //   },
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-  moduleDirectories: ['node_modules', '<rootDir>/'],
   moduleNameMapper: {
     '^/(.*)$': '<rootDir>/src/$1',
-
     /**
      * @todo add regex to handle global paths
      * @author yonycalsin
      */
-    '^assets/(.*)$': '<rootDir>/src/assets/$1',
-    '^typings/(.*)$': '<rootDir>/src/typings/$1',
-    '^mock-server/(.*)$': '<rootDir>/src/mock-server/$1',
-    '^analytics/(.*)$': '<rootDir>/src/analytics/$1',
-    '^services/(.*)$': '<rootDir>/src/services/$1',
-    '^themes/(.*)$': '<rootDir>/src/themes/$1',
-    '^hooks/(.*)$': '<rootDir>/src/hooks/$1',
-    '^screens/(.*)$': '<rootDir>/src/screens/$1',
-    '^layouts/(.*)$': '<rootDir>/src/layouts/$1',
-    '^containers/(.*)$': '<rootDir>/src/containers/$1',
-    '^components/(.*)$': '<rootDir>/src/components/$1',
-    '^utils/(.*)$': '<rootDir>/src/utils/$1',
+    '^typings(.*)$': '<rootDir>/src/typings$1',
+    '^assets(.*)$': '<rootDir>/src/assets$1',
+    '^mock-server(.*)$': '<rootDir>/src/mock-server$1',
+    '^analytics(.*)$': '<rootDir>/src/analytics$1',
+    '^services(.*)$': '<rootDir>/src/services$1',
+    '^themes(.*)$': '<rootDir>/src/themes$1',
+    '^hooks(.*)$': '<rootDir>/src/hooks$1',
+    '^screens(.*)$': '<rootDir>/src/screens$1',
+    '^layouts(.*)$': '<rootDir>/src/layouts$1',
+    '^containers(.*)$': '<rootDir>/src/containers$1',
+    '^components(.*)$': '<rootDir>/src/components$1',
+    '^utils(.*)$': '<rootDir>/src/utils$1',
 
     // Optionals
     '^~/server/(.*)$': '<rootDir>/server/$1',
   },
+  moduleDirectories: ['node_modules', '<rootDir>/'],
   testEnvironment: 'jest-environment-jsdom',
   snapshotSerializers: ['@emotion/jest/serializer'],
   collectCoverageFrom: [
@@ -57,8 +57,8 @@ const customJestConfig = {
     '!src/assets/**/*.*',
     '!src/utils/constants/*.*',
   ],
-  transformIgnorePatterns: ['/node_modules/', '\\.pnp\\.[^\\/]+$'],
   watchPlugins: ['jest-watch-typeahead/filename', 'jest-watch-typeahead/testname'],
+  verbose: true,
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
