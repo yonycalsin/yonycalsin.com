b26ec81f06733f9a787aa18540219b53c4b535fb
Yony Calsin <helloyonycalsin@gmail.com>
2022-10-16 18:53:15 -0500

    feat: add work item icon

--- src/screens/home/components/pinned-projects/project-card.tsx
+++ src/screens/home/components/pinned-projects/project-card.tsx
@@ -1,11 +1,20 @@
 import * as React from 'react'
-import { Box, Text } from '@chakra-ui/react'
+import { Box, HStack, Icon, Text } from '@chakra-ui/react'
 import RouterLink from 'next/link'
 
 import type { ProjectCardProps } from 'typings/screens'
+import { getWorkItemColors, getWorkItemIcon } from 'components/work-item/components'
 
 function ProjectCard(props: ProjectCardProps) {
   const { project } = props
+
+  const icon = React.useMemo(() => {
+    return getWorkItemIcon(project.type)
+  }, [project.type])
+
+  const colors = React.useMemo(() => {
+    return getWorkItemColors(project.type)
+  }, [project.type])
 
   return (
     <RouterLink href={`/projects#${project.slug}`} passHref>
@@ -27,9 +36,12 @@ function ProjectCard(props: ProjectCardProps) {
           backgroundColor: 'gray.800',
         }}
       >
-        <Text noOfLines={1} mb="1" fontWeight="semibold">
-          {project.name}
-        </Text>
+        <HStack>
+          <Icon as={icon} fill={colors.fill} />
+          <Text noOfLines={1} mb="1" fontWeight="semibold">
+            {project.name}
+          </Text>
+        </HStack>
         <Text fontSize="md" fontStyle="italic" textColor="gray" noOfLines={2}>
           {project.shortDescription}
         </Text>
