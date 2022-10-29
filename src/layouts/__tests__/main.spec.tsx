c25cf17b07a2eb5637e353279894085fe01e1951
Yony Calsin <helloyonycalsin@gmail.com>
2022-10-29 17:39:34 -0500

    test: update tests

--- src/layouts/__tests__/main.spec.tsx
+++ src/layouts/__tests__/main.spec.tsx
@@ -1,9 +1,18 @@
 import { ChakraKBar } from '@chakra-ui-kbar/core'
 import { render } from '@testing-library/react'
 
+import { overrideFeatures, TEST_ENVS } from 'tests'
 import { MainLayout } from 'layouts'
 
 describe('MainLayout', () => {
+  beforeAll(() => {
+    process.env = overrideFeatures()
+  })
+
+  afterAll(() => {
+    process.env = TEST_ENVS
+  })
+
   it('renders the main layout', () => {
     const view = render(
       <ChakraKBar>
