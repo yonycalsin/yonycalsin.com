8fa5358ffd816b5f91fe6038585f980792ede46c
Yony Calsin <helloyonycalsin@gmail.com>
2022-11-12 11:50:50 -0500

    feat: remove the layout from blog post screen

--- src/screens/blog-post/blog-post-screen.tsx
+++ src/screens/blog-post/blog-post-screen.tsx
@@ -7,7 +7,6 @@ import nextBase64 from 'next-base64'
 
 import type { BlogPostScreenProps } from 'typings/screens'
 import useMDXComponent from 'hooks/use-mdx-component'
-import { MainLayout } from 'layouts'
 import { Anchor } from 'components/anchor'
 import { Breadcrumb, BreadcrumbItem, BreadcrumbSeparator } from 'components/breadcrumb'
 import { Heading } from 'components/heading'
@@ -20,30 +19,26 @@ function BlogPostScreen(props: BlogPostScreenProps) {
   const Component = useMDXComponent(decodeURIComponent(decodeURIComponent(nextBase64.decode(post.body.code))))
 
   return (
-    <MainLayout>
-      <div className="container py-10 space-y-6">
-        <Breadcrumb>
-          <BreadcrumbItem>
-            <Link href="/" legacyBehavior passHref>
-              <Anchor>Home</Anchor>
-            </Link>
-          </BreadcrumbItem>
-          <BreadcrumbSeparator />
-          <BreadcrumbItem>
-            <Link href="/blog" legacyBehavior passHref>
-              <Anchor>Blog</Anchor>
-            </Link>
-          </BreadcrumbItem>
-        </Breadcrumb>
-        <header className="space-y-3">
-          <Heading size="h1">{post.title}</Heading>
-          <p className="italic text-gray-600">
-            Last updated at {dayjs(post.updatedAt).format(DATE_FORMATS.HUMAN_DATE)}
-          </p>
-        </header>
-        <Component components={MDXComponents} />
-      </div>
-    </MainLayout>
+    <div className="container py-10 space-y-6">
+      <Breadcrumb>
+        <BreadcrumbItem>
+          <Link href="/" legacyBehavior passHref>
+            <Anchor>Home</Anchor>
+          </Link>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator />
+        <BreadcrumbItem>
+          <Link href="/blog" legacyBehavior passHref>
+            <Anchor>Blog</Anchor>
+          </Link>
+        </BreadcrumbItem>
+      </Breadcrumb>
+      <header className="space-y-3">
+        <Heading size="h1">{post.title}</Heading>
+        <p className="italic text-gray-600">Last updated at {dayjs(post.updatedAt).format(DATE_FORMATS.HUMAN_DATE)}</p>
+      </header>
+      <Component components={MDXComponents} />
+    </div>
   )
 }
 
