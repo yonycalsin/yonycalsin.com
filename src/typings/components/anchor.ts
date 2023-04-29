b13abb4342792b7ff5ccf431fc6b0531c23ac34f
Yony Calsin <helloyonycalsin@gmail.com>
2023-04-28 21:44:30 -0500

    chore: upgrade next (#166)

--- src/typings/components/anchor.ts
+++ src/typings/components/anchor.ts
@@ -1,5 +1,8 @@
 import type * as React from 'react'
 
-type AnchorProps = React.AnchorHTMLAttributes<HTMLAnchorElement>
+type AnchorProps = React.AnchorHTMLAttributes<HTMLAnchorElement> & {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  href?: any
+}
 
 export type { AnchorProps }
