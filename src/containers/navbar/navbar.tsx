74a885b6037a6ca37715d782258a7d25d16ee729
Yony Calsin <helloyonycalsin@gmail.com>
2023-06-10 12:14:30 -0500

    feat: add dynamic backdrop to the navbar's menu

--- src/containers/navbar/navbar.tsx
+++ src/containers/navbar/navbar.tsx
@@ -7,34 +7,43 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 
+import { useDynamicBackdrop } from 'hooks'
+import { DynamicBackdrop } from 'components'
 import { cn } from 'utils'
 import { ENV } from 'utils/constants'
 
 function Navbar() {
   const pathname = usePathname()
 
+  const { backdropRef, handleMouseEnter, handleMouseLeave } = useDynamicBackdrop()
+
   return (
     <div className="w-full z-10 shadow-sm">
+      <DynamicBackdrop ref={backdropRef} />
       <div className="container py-5 md:py-6 flex justify-between items-center">
         <Link href="/">
           <Image className="border-2" src={isotipo} alt="Logo" width="40" height="40" />
         </Link>
-        <div className="flex items-center space-x-6 font-medium">
+        <div className="flex items-center space-x-1 font-medium">
           <Link
             href="/projects"
             className={cn(
-              'transition-colors hover:text-foreground/80',
+              'transition-colors hover:text-foreground/80 px-2',
               pathname === '/projects' ? 'text-foreground' : 'text-foreground/60',
             )}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
           >
             Projects
           </Link>
           <Link
             href="/me"
             className={cn(
-              'transition-colors hover:text-foreground/80',
+              'transition-colors hover:text-foreground/80 px-2',
               pathname === '/me' ? 'text-foreground' : 'text-foreground/60',
             )}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
           >
             About Me
           </Link>
@@ -42,9 +51,11 @@ function Navbar() {
             <Link
               href="/faq"
               className={cn(
-                'transition-colors hover:text-foreground/80',
+                'transition-colors hover:text-foreground/80 px-2',
                 pathname === '/faq' ? 'text-foreground' : 'text-foreground/60',
               )}
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
             >
               FAQ
             </Link>
@@ -53,9 +64,11 @@ function Navbar() {
             <Link
               href="/uses"
               className={cn(
-                'transition-colors hover:text-foreground/80 hidden lg:inline-block',
+                'transition-colors hover:text-foreground/80 px-2 hidden lg:inline-block',
                 pathname === '/uses' ? 'text-foreground' : 'text-foreground/60',
               )}
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
             >
               Uses
             </Link>
@@ -64,9 +77,11 @@ function Navbar() {
             <Link
               href="/blog/categories/snippet"
               className={cn(
-                'transition-colors hover:text-foreground/80 hidden lg:inline-block',
+                'transition-colors hover:text-foreground/80 px-2 hidden lg:inline-block',
                 pathname?.startsWith('/blog/categories/snippet') ? 'text-foreground' : 'text-foreground/60',
               )}
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
             >
               Snippets
             </Link>
--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,2 +1,3 @@
+export { default as useDynamicBackdrop } from './use-dynamic-backdrop'
 export { default as useMDXComponent } from './use-mdx-component'
 export { default as useQueryCache } from './use-query-cache'
