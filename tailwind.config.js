5bab750a5cc4bce3991541229a4024e564639e3b
Yony Calsin <helloyonycalsin@gmail.com>
2023-10-08 12:16:24 -0500

    feat: change the main font to inter

--- tailwind.config.js
+++ tailwind.config.js
@@ -73,6 +73,9 @@ module.exports = {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
       },
+      fontFamily: {
+        sans: 'var(--font-inter)',
+      },
     },
   },
   plugins: [require('tailwindcss-animate')],
