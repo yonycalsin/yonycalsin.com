4967dbc2e9ce7e4e4e1c550f46ef61132adb146a
Yony Calsin <58490737+yonycalsin@users.noreply.github.com>
2024-01-20 10:26:49 -0500

    feat: add vercel speed insights

--- src/analytics/vercel-insights/vercel-insights.tsx
+++ src/analytics/vercel-insights/vercel-insights.tsx
@@ -1,10 +1,14 @@
-'use client'
-
 import * as React from 'react'
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 function VercelInsights() {
-  return <Analytics />
+  return (
+    <>
+      <Analytics />
+      <SpeedInsights />
+    </>
+  )
 }
 
 export default VercelInsights
