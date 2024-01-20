6a170ef1197ae03e0f29958f774908996901d7b3
Yony Calsin <58490737+yonycalsin@users.noreply.github.com>
2024-01-19 23:57:52 -0500

    feat: improve the LCP metric

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,13 @@
 import * as React from 'react'
 
 import HomeScreen from 'screens/home'
-import { PageTransition } from 'components'
 
 export const metadata = {
   robots: 'index, follow',
 }
 
 function HomePage() {
-  return (
-    <PageTransition>
-      <HomeScreen />
-    </PageTransition>
-  )
+  return <HomeScreen />
 }
 
 export default HomePage
